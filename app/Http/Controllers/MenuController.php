415c5cec68e5b6bf014d0954757a8365fe29e5de
BM <monnybram@outlook.com>
2026-04-25 16:24:11 +0900

    Tampilkan data Menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\Item;
+
+use function Pest\Laravel\session;
+
+class MenuController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tableNumber = $request->query('meja');
+        if ($tableNumber) {
+            Session::put('tableNumber', $tableNumber);
+        }
+
+        $items = Item::where('is_active', 1)->orderBy('name', 'asc')->get();
+
+        return view('customer.menu', compact('items', 'tableNumber'));
+    }
+}
--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -15,7 +15,13 @@ class ItemFactory extends Factory
             'description' => $this->faker->text(),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'category_id' => $this->faker->numberBetween(1, 2),
-            'img' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(
+                [
+                    'https://plus.unsplash.com/premium_photo-1694670234085-4f38b261ce5b'.
+                    'https://plus.unsplash.com/premium_photo-1694547926001-f2151e4a476b',
+                    'https://images.unsplash.com/photo-1591325418441-ff678baf78ef',
+                    'https://images.unsplash.com/photo-1638866281450-3933540af86a'
+                ]),
             'is_active' => $this->faker->boolean()
         ];
     }
