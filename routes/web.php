68f95ce7b9b7a9620cae9fd86906e534a66322a6
BM <monnybram@outlook.com>
2026-04-25 11:00:56 +0900

    Integrasi dan Layouting Template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/menu', function () {
+    return view('customer.menu');
+})->name('menu');
+
+Route::get('/menu/cart', function () {
+    return view('customer.cart');
+})->name('cart');
+
+Route::get('/menu/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
