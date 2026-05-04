806f48f68527498234ef4979744ffb10aeb3db76
BM <monnybram@outlook.com>
2026-05-04 22:50:52 +0900

    Create, Update & Delete Role Done

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -7,60 +7,57 @@ use App\Models\Role;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $roles = Role::all();
         return view('admin.role.index', compact('roles'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        
+        return view('admin.role.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'role_name' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        // Jika berhasil
+
+        Role::create($validate);
+
+        return redirect()->route('roles.index')->with('success', 'Role : ' . $validate['role_name'] . ', created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function edit(Role $role)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return view('admin.role.edit', compact('role'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $validate = $request->validate([
+            'role_name' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        // Jika berhasil validasi, maka update data
+
+        $role->update($validate);
+
+        return redirect()->route('roles.index')->with('success', 'Role : ' . $validate['role_name'] . ', updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        //
+        $role->delete();
+
+        return redirect()->route('roles.index')->with('success', 'Role : ' . $role->role_name . ', Deleted successfully.');
     }
 }
---
+++ resources/views/admin/role/edit.blade.php
@@ -0,0 +1,102 @@
+@extends('admin.layouts.master')
+@section('title', 'Manajemen Role')
+
+@section('content')
+
+<div class="page-heading">
+  <div class="page-title">
+    <div class="row">
+      <div class="col-12 col-md-6 order-md-1 order-last">
+        {{-- <h3>Form Validation</h3> --}}
+        <p class="text-subtitle text-muted">
+          {{-- Complete the form with powerful validation library such as Parsley. --}}
+        </p>
+      </div>
+      <div class="col-12 col-md-6 order-md-2 order-first">
+        <nav
+          aria-label="breadcrumb"
+          class="breadcrumb-header float-start float-lg-end"
+        >
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Manajemen Role
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Update Role</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <!-- // Basic multiple Column Form section start -->
+  <section id="multiple-column-form">
+    <div class="row match-height">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Update Role</h3>
+          </div>
+          <div class="card-content">
+            <div class="card-body">
+              <form class="form" action="{{ route('roles.update', $role->id) }}" method="POST">
+                 @csrf
+                @method('PUT')
+
+                <div class="row">
+                  <div class="col-md-6 col-12">
+                    <div class="form-group mandatory">
+                      <label for="role_name" class="form-label"
+                        >Role Name</label
+                      >
+                      <input
+                        type="text"
+                        id="role_name"
+                        class="form-control"
+                        placeholder="Role Name"
+                        name="role_name"
+                        value="{{ old('role_name', $role->role_name) }}"
+                        required
+                      />
+                        @error('role_name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                  </div>
+                  <div class="col-md-6 col-12">
+                    <div class="form-group">
+                      <label for="description" class="form-label"
+                        >Description</label
+                      >
+                      <input
+                        type="text"
+                        id="description"
+                        class="form-control"
+                        placeholder="Description"
+                        name="description"
+                        value="{{ old('description', $role->description) }}"
+                        required
+                      />
+                        @error('description')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12 d-flex justify-content-end">
+                    <button type="submit" class="btn btn-success">Update Role</button>
+                    <a href="{{ route('roles.index') }}" class="btn btn-danger ms-2">Cancel</a>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- // Basic multiple Column Form section end -->
+</div>
+
+@endsection
