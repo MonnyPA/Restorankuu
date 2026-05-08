c227324d76008405f6c35f92185aa4c99fd34a42
BM <monnybram@outlook.com>
2026-05-09 00:56:16 +0900

    Install Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,20 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\MenuController;
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route Customer
 Route::get('/', function () {
-    return redirect()->route('menu');});
+    return view('welcome');
+});
 
-Route::get('/menu', [MenuController::class, 'index'])->name('menu');
-Route::get('/cart', [MenuController::class, 'cart'])->name('cart');
-Route::post('/cart/add', [MenuController::class, 'addToCart'])->name('cart.add');
-Route::post('/cart/update', [MenuController::class, 'updateCart'])->name('cart.update');
-Route::post('/cart/remove', [MenuController::class, 'removeCart'])->name('cart.remove');
-Route::get('/cart/clear', [MenuController::class, 'clearCart'])->name('cart.clear');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/checkout', [MenuController::class, 'checkout'])->name('checkout');
-Route::post('/checkout/store', [MenuController::class, 'storeOrder'])->name('checkout.store');
-Route::get('/checkout/success/{orderId}', [MenuController::class, 'checkoutSuccess'])->name('checkout.success');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route Admin
-Route::get('/dashboard', function(){
-    return view('admin.dashboard');
-})->name('dashboard');
-
-Route::resource('categories', CategoryController::class);
-Route::resource('users', UserController::class);
-
-Route::resource('items', ItemController::class);
-Route::get('/items/active/{id}', [ItemController::class, 'markAsActive'])->name('items.active');
-Route::get('/tasks/nonactive/{id}', [ItemController::class, 'markAsNonactive'])->name('items.nonactive');
-
-Route::resource('orders', OrderController::class);
-Route::get('/orders/settlement/{id}', [OrderController::class, 'markAsSettled'])->name('orders.settlement');
-
-Route::resource('roles', RoleController::class);
-
-
-
-
+require __DIR__.'/auth.php';
